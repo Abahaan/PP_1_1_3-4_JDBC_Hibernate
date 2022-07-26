8facd48cd837ffcdd8d8c27c3ec371ef28a3d3f7
abahaan.kokov@gmail.com <pOj65xT69>
2022-07-26 23:36:02 +0700

    Util - config of mySQL connection

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,36 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String USER = "root";
+    private static final String PASSWORD = "1234";
+    private static final String URL = "jdbc:mysql://localhost:3306/pre-project1_1";
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName(DB_DRIVER);
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+        return connection;
+    }
+    public static void closeConnection() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
