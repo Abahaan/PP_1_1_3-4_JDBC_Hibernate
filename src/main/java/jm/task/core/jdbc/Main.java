f061318c5e0805e58a70e695466291c9cb38c887
abahaan.kokov@gmail.com <pOj65xT69>
2022-07-28 22:23:48 +0700

    the conditions of the task are met

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,28 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+        userService.saveUser("Peter", "Parker", (byte) 22);
+        userService.saveUser("Sergei", "Sergeev", (byte) 41);
+        userService.saveUser("Gena", "Kokov", (byte) 28);
+        userService.saveUser("Hope", "Pinkman", (byte) 19);
+        for (User user : userService.getAllUsers()) {
+            System.out.println(user.toString());
+        }
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
