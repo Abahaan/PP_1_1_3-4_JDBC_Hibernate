aacfbc397c3d0cda527fbfc232b8e31a71ee8543
abahaan.kokov@gmail.com <pOj65xT69>
2022-07-30 19:25:43 +0700

    UTIL hibernate config

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,16 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
@@ -10,6 +18,36 @@ public class Util {
     private static final String PASSWORD = "1234";
     private static final String URL = "jdbc:mysql://localhost:3306/pre-project1_1";
     private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                Properties properties = new Properties();
+                properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                properties.put(Environment.URL, "jdbc:mysql://localhost:3306/pre-project1_1");
+                properties.put(Environment.USER, "root");
+                properties.put(Environment.PASS, "1234");
+                properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                properties.put(Environment.SHOW_SQL, "true");
+
+                configuration.setProperties(properties);
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry =
+                        new StandardServiceRegistryBuilder()
+                                .applySettings(configuration.getProperties())
+                                .build();
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);//
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
 
     private static Connection connection;
 
